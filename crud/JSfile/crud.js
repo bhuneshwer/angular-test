d538181bf8d5ea755e31056a42a85803cce7c3a7
shigufa-aaliya <shigufaaaliya@gmail.com>
2017-04-06 15:54:27 +0530

    hi

--- crud/JSfile/crud.js
+++ crud/JSfile/crud.js
@@ -1,66 +1,56 @@
  var app = angular.module('mainApp', ['ngRoute']);
 
- var items=[]
- var user={};
+ var items = []
+ var user = {};
 
-app.config(function($routeProvider) {
-             $routeProvider
-             .when('/index', {
-                     templateUrl: 'partial/index.html',
-                     controller: 'mainController'
-                 })
-                 .when('/view/:id', {
-                     templateUrl: 'partial/view.html',
-                     controller: 'detailController'
-                 }).
-                 otherwise({
-                    redirectTo:'/index'
-                 });
-         });
+ app.config(function($routeProvider, $locationProvider) {
+     $locationProvider.hashPrefix('');
+     $routeProvider
+         .when('/index', {
+             templateUrl: 'partial/index.html',
+             controller: 'mainController'
+         })
+         .when('/view/:id', {
+             templateUrl: 'partial/view.html',
+             controller: 'detailController'
+         }).
+     otherwise({
+         redirectTo: '/index'
+     });
+ });
 
  app.controller("mainController", function($scope, $location) {
      $scope.items = items;
-     $scope.user=user;
-     $scope.selectedRow =null;
+     $scope.user = user;
+     $scope.selectedRow = null;
      $scope.addItem = function(item) {
-             $scope.items.push(item);
-             $scope.item = {};
-         },
-         $scope.removeItem = function(index) {
-             console.log(index);
-             $scope.items.splice(index, 1)
-         },
-         $scope.editItem = function(index) {
-             $scope.editing = $scope.items.indexOf(index);
-         };
-
-     $scope.setClickedRow= function(index){
-        // $scope.selectedRow =index;
-        // console.log(items[index]);
-        user=items[index];
-        console.log(user);        
-        $location.path('/view')
+         $scope.items.push(item);
+         $scope.item = {};
      }
- });
-app.controller("detailController", function($scope, $location,$routeParams) {
-    
-     $scope.user=user;
-     for(var i=0;i<items.length;i++)
-     {
-     
-     if(items[i].email==$routeParams.id)
-     {
-        $scope.user=items[i];
+     $scope.removeItem = function(index) {
+         console.log(index);
+         $scope.items.splice(index, 1)
      }
-    }
-    
+     $scope.editItem = function(index) {
+         $scope.editing = $scope.items.indexOf(index);
+     };
  });
-// app.controller("detailController",['$scope','$http','$routeParams',
-//      function($scope, $http, $routeParams)
-//         {    
-//             $http.get('js/data.json').success (function(data){
-//                 $scope.user = data;
-//                 $scope.aee = $routeParams.id;
-//             }); 
-//         }])
+ app.controller("detailController", function($scope, $location, $routeParams) {
+     console.log($routeParams.id)
+     $scope.user = user;
+     for (var i = 0; i < items.length; i++) {
 
+         if (items[i].email == $routeParams.id) {
+             $scope.user = items[i];
+         }
+     }
+
+ });
+ // app.controller("detailController",['$scope','$http','$routeParams',
+ //      function($scope, $http, $routeParams)
+ //         {    
+ //             $http.get('js/data.json').success (function(data){
+ //                 $scope.user = data;
+ //                 $scope.aee = $routeParams.id;
+ //             }); 
+ //         }])
